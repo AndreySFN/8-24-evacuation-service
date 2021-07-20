2ba13c2d372c8d0433a8c53e96f919aa6eca1a1d
AndreySFN <andreysfn@yanex.ru>
2021-07-20 19:26:12 +0300

    Добавить отключение обратного звонка при недоступности API или невозможности вызова

--- src/components/Recall.tsx
+++ src/components/Recall.tsx
@@ -1,14 +1,33 @@
 // !!!
 /* eslint-disable */
-import React, {useState} from "react";
+import React, {useEffect, useState} from "react";
 import "./recall.scss"
+
 const Recall = () => {
     const [phone, setPhone] = useState("");
+    const [isEnabled, setIsEnabled] = useState(false);
     const [isSent, setIsSent] = useState(false);
     const [errorMessage, setErrorMessage] = useState("");
+    useEffect(() => {
+            // @ts-ignore
+            if (window.jivo_api) {
+                // @ts-ignore
+                window.jivo_api.isCallbackEnabled((res) => {
+                    setIsEnabled(res.result.includes('ok'))
+                    if (!res.result.includes('ok')) {
+                        setErrorMessage('В настоящий момент обратный звонок недоступен')
+                    }
+                })
+            } else {
+                setErrorMessage('В настоящий момент обратный звонок недоступен')
+            }
+        },
+        // @ts-ignore
+        [window.jivo_api]
+    )
     const handleInput = (e: any) => {
         setErrorMessage("");
-            setPhone(e.target.value)
+        setPhone(e.target.value)
     }
     const handleSubmit = () => {
         // !!!
@@ -20,16 +39,16 @@ const Recall = () => {
         }
         return false;
     }
-    const renderForm = () => <div className='recall_form'>
-            <input id="recall_input" onChange={handleInput} placeholder={"+7 (___) ___-__-__"}
-                   value={phone}/>
-        <button id="recall_submit" onClick={handleSubmit}>ЗАКАЗАТЬ ЗВОНОК</button>
+    const renderForm = (disabled: boolean) => <div className='recall_form'>
+        <input id="recall_input" onChange={handleInput} disabled={disabled} placeholder={"+7 (___) ___-__-__"}
+               value={phone}/>
+        <button id="recall_submit" disabled={disabled} onClick={handleSubmit}>ЗАКАЗАТЬ ЗВОНОК</button>
     </div>
 
     const renderMessage = () => <span id='recall_success'>Мы уже перезваниваем вам!</span>
 
     return <div id="recall">
-        {isSent ? renderMessage() : renderForm()}
+        {isSent ? renderMessage() : renderForm(!isEnabled)}
         <div className='errorMessage'>{errorMessage}</div>
     </div>
 }
