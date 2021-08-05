bdb97679ec1eed79f6f004aaf5649befb55f510f
AndreySFN <AndreySFN@yanex.ru>
2021-08-05 20:31:20 +0300

    Исправление ошибки типов react-router-dom

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,6 @@ import React from 'react';
 import Header from "./components/Header";
 import {Switch, Route} from 'react-router-dom'
 import Main from "./forms/Main";
-import Prices from "./forms/Prices";
 import Contacts from "./forms/Contacts";
 import UnderConstruction from "./forms/UnderConstruction";
 
